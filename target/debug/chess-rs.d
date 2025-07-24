c80a01d5108e49675e9218fdafe401bb718ad5f2
Harshit-Dhanwalkar <harshitpd1729@gmail.com>
2025-07-24 10:38:35 +0530

    remove target

--- target/debug/chess-rs.d
+++
@@ -1,1 +0,0 @@
-/home/harshitpd/Documents/GITHUB/Chess-rs/chess-rs/target/debug/chess-rs: /home/harshitpd/Documents/GITHUB/Chess-rs/chess-rs/src/main.rs
--- target/debug/deps/chess_rs-aa8adaeb2b5d92c5.d
+++
@@ -1,5 +0,0 @@
-/home/harshitpd/Documents/GITHUB/Chess-rs/chess-rs/target/debug/deps/chess_rs-aa8adaeb2b5d92c5.d: src/main.rs
-
-/home/harshitpd/Documents/GITHUB/Chess-rs/chess-rs/target/debug/deps/libchess_rs-aa8adaeb2b5d92c5.rmeta: src/main.rs
-
-src/main.rs:
